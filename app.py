4840ff6500f6180fa60275c5373441d7bc820cd1
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-11 16:21:04 -0300

    remove if

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def index():
     url = random.choice(images)
     return render_template('index.html', url=url)
 
-if if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
